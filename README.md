12e682b4a0ab7bf8117f6ad223745db1d867a421
P. Scott DeVos <scott.devos@sykes.com>
2019-11-12 07:22:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# pyparse_datetime
+
+Timezone aware now() function and a parse_datetime function to make sure all your
+datetimes are timezone aware.
---
+++ dt.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+import pytz
+from dateutil.parser import parse
+from iso8601 import parse_date, ParseError
+
+
+def now(tzinfo=None):
+    """returns and aware datetime representing the current date and time
+    in the timezone provided by tzinfo (or pytz.UTC if tzinfo is None)
+    """
+    tzinfo = tzinfo or pytz.utc
+    return parse_datetime(datetime.utcnow(), tzinfo)
+
+
+def parse_datetime(value, tzinfo=None):
+    '''returns an aware datetime in the timezone specified by tzinfo.
+    If tzinfo is not supplied or is None, the resulting datetime will
+    be in the pytz.UTC or iso8601.UTC timezone (as described below)
+
+    if value is a string, it must be iso8601 compatible. If the offset is
+    not specified the resulting aware datetime will use tzinfo if provided
+    otherwise it will use iso8601.UTC
+
+    if value is a naive datetime, it will be assumed to be in UTC and will
+    then be converted to the timezone specified by tzinfo (if provided)
+
+    if value is an aware datetime, it will be converted to an aware datetime
+    in pytz.UTC or in tzinfo it if is provided.
+    '''
+    if isinstance(value, str):
+        try:
+            aware_dt = parse_date(value)
+        except ParseError:
+            aware_dt = pytz.utc.localize(parse(value))
+    else:
+        aware_dt = value if value.tzinfo else pytz.utc.localize(value)
+    if tzinfo:
+        return aware_dt.astimezone(tzinfo)
+    return aware_dt.astimezone(pytz.utc)
